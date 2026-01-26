4f862c48e6e2c9705a13d59549792b967625d671
PresentKim <debe3721@gmail.com>
2026-01-27 02:28:55 +0900

    feat(kotlin): Update kotlin to 2.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,8 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import java.util.*
 
 plugins {
-    kotlin("jvm") version "2.0.0"
+    kotlin("jvm") version "2.3.0"
+    kotlin("plugin.serialization") version "2.3.0"
     id("com.gradleup.shadow") version "8.3.0"
 }
 
@@ -23,6 +24,8 @@ repositories {
 dependencies {
     compileOnly("org.powernukkitx:server:2.0.0-SNAPSHOT")
     compileOnly(kotlin("stdlib"))
+    compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
+    compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
 
     implementation("org.jetbrains.exposed:exposed-core:0.61.0")
     implementation("org.jetbrains.exposed:exposed-dao:0.61.0")
